d7b00b7cb70c72e3a80957b5c9cddbed061837a0
sangyup <sangyeobi712@gmail.com>
2026-05-17 07:40:15 +0900

    뷰연결

--- SOPKATHON-38-iOS2/SOPKATHON-38-iOS2/Presentation/PostDetailView/PostDetailViewController.swift
+++ SOPKATHON-38-iOS2/SOPKATHON-38-iOS2/Presentation/PostDetailView/PostDetailViewController.swift
@@ -12,14 +12,21 @@ import Then
 
 final class PostDetailViewController: BaseViewController {
     
-    // MARK: - UI Components
-    
-    private let rootView = PostContentsView()
+    private let rootView = UIView()
+    private let postContentsView = PostContentsView()
     private let bubbleMessageView = BubbleMessageView()
     private let grayLineView = UIView()
     private let commentBoxView = UIView()
     private let postCommentView = PostCommentView()
-
+    
+    override var contentRootView: UIView? {
+        return rootView
+    }
+    
+    override var shouldShowNavigationBar: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,13 +35,26 @@ final class PostDetailViewController: BaseViewController {
         setLayout()
     }
     
-    override var shouldShowNavigationBar: Bool {
-        return true
+    override func configureNavigationBar() {
+        navigationBar.configure(
+            title: "전문 읽기",
+            showBackButton: true
+        )
+        
+        navigationBar.backButtonAction = { [weak self] in
+            guard let self else { return }
+            
+            if let navigationController {
+                navigationController.popViewController(animated: true)
+            } else {
+                dismiss(animated: true)
+            }
+        }
     }
     
     private func setStyle() {
-        
         view.backgroundColor = .white
+        rootView.backgroundColor = .white
         
         grayLineView.do {
             $0.backgroundColor = .gray200
@@ -42,19 +62,16 @@ final class PostDetailViewController: BaseViewController {
         
         commentBoxView.do {
             $0.backgroundColor = .white
-            
             $0.layer.shadowColor = UIColor.white.cgColor
             $0.layer.shadowOpacity = 0.7
             $0.layer.shadowRadius = 12
             $0.layer.shadowOffset = CGSize(width: 0, height: -42)
-
         }
     }
     
     private func setUI() {
-        
-        view.addSubviews(
-            rootView,
+        rootView.addSubviews(
+            postContentsView,
             bubbleMessageView,
             grayLineView,
             commentBoxView
@@ -62,37 +79,27 @@ final class PostDetailViewController: BaseViewController {
         
         commentBoxView.addSubview(postCommentView)
     }
-    // MARK: - Custom Methods
-
-    override func configureNavigationBar() {
-        navigationBar.configure (
-            title: "전문 읽기",
-            showBackButton: true
-        )
-    }
     
     private func setLayout() {
-        
-        rootView.snp.makeConstraints {
-            $0.top.equalTo(navigationBar.snp.bottom)
-            $0.horizontalEdges.equalToSuperview()
+        postContentsView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
             $0.height.equalTo(479)
         }
         
         bubbleMessageView.snp.makeConstraints {
-            $0.top.equalTo(rootView.snp.bottom)
-            $0.leading.trailing.equalToSuperview()
+            $0.top.equalTo(postContentsView.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalTo(commentBoxView.snp.top)
         }
         
         grayLineView.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
             $0.bottom.equalTo(commentBoxView.snp.top)
             $0.height.equalTo(1)
         }
         
         commentBoxView.snp.makeConstraints {
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.horizontalEdges.bottom.equalToSuperview()
             $0.height.equalTo(84)
         }
         
