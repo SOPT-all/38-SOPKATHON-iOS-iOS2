6bea38f2c11aa7463e5ea76047ca3fbb430aa249
ser0kim <srkim0728@gmail.com>
2026-05-17 02:09:16 +0900

    [feat] #4 - 네비게이션바 Test

---
+++ SOPKATHON-38-iOS2/SOPKATHON-38-iOS2/Presentation/PostDetailView/PostContentView.swift
@@ -0,0 +1,15 @@
+//
+//  PostContentView.swift
+//  SOPKATHON-38-iOS2
+//
+//  Created by 김세령 on 5/17/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class PostContentView: UIView {
+    
+}
